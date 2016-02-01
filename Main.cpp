ab8fa04d6a9ceaea56137488ff1935601bef5fc7
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2016-02-01 17:26:16 +0000

    Adding the creation of the calculator in the Main file

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,15 @@
 #include <QMainWindow>
 #include "MainWindow.h"
 
+#include "BinomialCalculator.h"
+
 int main(int argc, char **argv)
 {
   QApplication a(argc, argv);  
   MainWindow *window = new MainWindow();
+  BinomialCalculator* calculator = new BinomialCalculator();
+  window->SetCalculator (calculator);
+  
   window->setWindowTitle(QString::fromUtf8("Negitiation Study"));
   window->resize(336, 227);
   window->show();
