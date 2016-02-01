9acd17e59270abafef077fb33abae5614c2fd6cb
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2016-02-01 20:17:52 +0000

    Change the window size for optimal visual results

--- Main.cpp
+++ Main.cpp
@@ -10,9 +10,7 @@ int main(int argc, char **argv)
   MainWindow *window = new MainWindow();
   BinomialCalculator* calculator = new BinomialCalculator();
   window->SetCalculator (calculator);
-  
-  window->setWindowTitle(QString::fromUtf8("Negitiation Study"));
-  window->resize(336, 227);
+  window->resize(600, 600);
   window->show();
   return a.exec();
 }
