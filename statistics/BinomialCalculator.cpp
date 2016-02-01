557199f5db487cc5b59e43b8f59db086cfa4d12d
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2016-02-01 20:13:47 +0000

    bug-fix: when the number of experiments is large, binomial coefficient calculation fails and needs to be replaced by Normal distribution approximation

--- statistics/BinomialCalculator.cpp
+++ statistics/BinomialCalculator.cpp
@@ -1,5 +1,6 @@
 #include "BinomialCalculator.h"
 #include <boost/math/special_functions/binomial.hpp>
+#include <boost/math/distributions/normal.hpp>
 
 #include <cmath>
 #include <iostream>
@@ -21,7 +22,22 @@ float BinomialCalculator::GetProbabilityMassFunction (unsigned int k) const
   }
   
   float p = this->ProbabilityOfSuccess;
-  float C = boost::math::binomial_coefficient<float> (n, k);
-  float mass = C * std::pow (p, k) * std::pow(1.0 - p, n - k);
+
+  float mass = 0.0;
+  
+  if (n > 100)
+  {
+    // Use the Normal approximation for the mass function to avoid overflow
+    float normal_mean = n * p;
+    float normal_sd = std::sqrt (n * p * (1 - p));
+    mass = boost::math::pdf (boost::math::normal(normal_mean, normal_sd), k);
+  }
+  else
+  {  
+    // Use the binomial distribution for small observation numbers
+    float C = boost::math::binomial_coefficient<float> (n, k);
+    mass = C * std::pow (p, k) * std::pow(1.0 - p, n - k);
+  }
+  
   return mass;
 }
